0d61997bb0f302d167487c37f7bf91e36415bee3
= <=>
2024-12-27 15:55:33 -0300

    button fix

--- portafoliov2/src/UpRight.js
+++ portafoliov2/src/UpRight.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const UpRight = () => {
+
+  const handleContact = () =>{
+    alert('No funciona todavia pero mi mail es lionelperafan@gmail.com')
+  }
+
   return (
     <div className="upRight">
       <div className="title">Desarrollador Backend</div>
-      <div className="contactme">Contactame</div>
+      <div className="contactme" onClick={handleContact}>Contactame</div>
     </div>
   );
 };
